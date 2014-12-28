fe2c0cd730c9069743054cdfbc6adbd09fec7b45
miffylf <miffylf@outlook.com>
2014-12-28 10:39:21 +0800

    添加anaysis和download

--- spider/Anaysis.cs
+++ spider/Anaysis.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Collections;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace spider
 {
-    class Anaysis
+    public class Anaysis
     {
+        public void GetHerf(Hashtable hashTable,string url)
+        {
+            HtmlWeb htmlweb = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument document = htmlweb.Load(url);
+            var linksOnPage = from lnks in document.DocumentNode.Descendants()
+                              where lnks.Name == "a" &&
+                              lnks.Attributes["href"] != null &&
+                              lnks.InnerText.Trim().Length > 0
+                              select new
+                              {
+                                  Url = lnks.Attributes["href"].Value,
+                                  Text = lnks.InnerText
+                              };
+            foreach (var item in linksOnPage)
+            {
+                hashTable.Add(item.Text, item.Url);
+                Console.WriteLine(item);
+            }
+        }
     }
 }
--- spider/DownLoadHtml.cs
+++ spider/DownLoadHtml.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security;
+using System.Net;
+using System.IO;
 
 namespace spider
 {
-    class DownLoadHtml
+    public class DownLoadHtml
     {
+        //public bool InsertHtml(string url)
+        //{
+        //    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        //    WebResponse response = request.GetResponse();
+        //    if (!response.ContentType.ToLower().StartsWith("/text"))
+        //    {
+
+        //    }
+        //}
     }
 }
